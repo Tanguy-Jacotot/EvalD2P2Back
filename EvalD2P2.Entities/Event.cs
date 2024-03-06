cb8429b7791c477d6de8edcbef4410b5fddc6ab6
TanguyDiiage <tanguy.jacotot@diiage.org>
2024-03-06 13:25:24 +0100

    Add event model - title, description, date, location

---
+++ EvalD2P2.Entities/Event.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvalD2P2.Entities;
+
+public class Event
+{
+    [Key]
+    public Guid IdEvent { get; set; } = Guid.NewGuid();
+    
+    [MaxLength(50)]
+    public string? Title { get; set; }
+    
+    [MaxLength(50)]
+    public string? Description { get; set; }
+    
+    public DateTime Date { get; set; }
+    
+    [MaxLength(50)]
+    public string? Location { get; set; }
+}
